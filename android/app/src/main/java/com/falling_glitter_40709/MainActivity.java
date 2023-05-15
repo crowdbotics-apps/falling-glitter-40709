71db486d820477dbaf8bcaedbd77c5dbb3a15471
Crowdbotics <team@crowdbotics.com>
2023-05-15 18:33:06 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/falling_glitter_40709/MainActivity.java
@@ -0,0 +1,15 @@
+package com.falling_glitter_40709;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "falling_glitter_40709";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "falling-glitter-40709",
+  url: "https://falling-glitter-40709.botics.co",
+  api: "https://falling-glitter-40709.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
